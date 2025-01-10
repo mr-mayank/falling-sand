d1043cf49cf69eea105b42c9c907add97df5373a
mayankkumar.patel <mayank.patel@rapidops.co>
2025-01-10 17:05:15 +0530

    added functionality realtime update on launch page while joining leaving and kick player

--- src/pages/battleship/launch-page/launch-page-controller.ts
+++ src/pages/battleship/launch-page/launch-page-controller.ts
@@ -2,7 +2,9 @@ import { useNavigate, useParams } from "react-router-dom";
 import { useTheme } from "../../../context/theme-context";
 import { useEffect, useState } from "react";
 import { useUser } from "../../../context/user-context";
-import { useGetGame } from "../service";
+import { useGetGame, useKickPlayer, useLeaveGame } from "../service";
+import { io, Socket } from "socket.io-client";
+import { toast } from "react-toastify";
 
 interface GameRoom {
   id: string;
@@ -25,8 +27,11 @@ const useLaunchPageController = () => {
   const [showUrlTooltip, setShowUrlTooltip] = useState(false);
   const [showKickConfirm, setShowKickConfirm] = useState(false);
   const [gameRoomDetails, setGameRoomDetails] = useState<GameRoom | null>(null);
+  const [socket, setSocket] = useState<Socket | null>(null);
   const { user } = useUser();
   const getGame = useGetGame(id);
+  const leaveGame = useLeaveGame();
+  const kickPlayer = useKickPlayer();
 
   const handleCopyLink = () => {
     const gameUrl = window.location.href;
@@ -36,7 +41,10 @@ const useLaunchPageController = () => {
   };
 
   const handleLeaveGame = () => {
-    navigate("/battleship");
+    leaveGame.mutate({
+      roomID: gameRoomDetails?.name || "",
+      player: user?.id || "",
+    });
   };
 
   const handleStartGame = () => {
@@ -46,6 +54,10 @@ const useLaunchPageController = () => {
   const handleCopyLeave = () => setShowUrlTooltip(false);
   const handleKickPlayer = () => {
     setShowKickConfirm(false);
+    kickPlayer.mutate({
+      roomID: gameRoomDetails?.name || "",
+      player: gameRoomDetails?.player2?.id || "",
+    });
   };
 
   useEffect(() => {
@@ -68,6 +80,73 @@ const useLaunchPageController = () => {
     }
     //eslint-disable-next-line react-hooks/exhaustive-deps
   }, [getGame.isSuccess, getGame.data]);
+
+  useEffect(() => {
+    if (kickPlayer.isSuccess) {
+      socket?.emit("kickPlayer", {
+        kickedPlayerId: gameRoomDetails?.player2?.id,
+        gameId: id,
+      });
+      setShowKickConfirm(false);
+      getGame.refetch();
+    }
+    //eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [kickPlayer.isSuccess, kickPlayer.data]);
+
+  useEffect(() => {
+    if (leaveGame.isSuccess) {
+      socket?.emit("leaveGame", { playerId: user?.id, gameId: id });
+
+      navigate("/battleship");
+    }
+    //eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [leaveGame.isSuccess, leaveGame.data]);
+
+  useEffect(() => {
+    const newSocket = io(process.env.REACT_APP_API_URL);
+    setSocket(newSocket);
+
+    return () => {
+      newSocket.close();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!socket) return;
+
+    // Join the game room
+    socket.emit("joinGame", { playerId: user?.id, gameId: id });
+
+    // Listen for player updates
+    socket.on("playerJoined", () => {
+      getGame.refetch();
+    });
+
+    socket.on("playerLeft", ({ playerId }: { playerId: string }) => {
+      if (playerId === gameRoomDetails?.player1.id) {
+        toast("Host has left the Game ");
+        navigate("/battleship");
+      } else {
+        getGame.refetch();
+      }
+    });
+
+    socket.on("playerKicked", ({ kickedPlayerId }) => {
+      if (kickedPlayerId === user?.id) {
+        toast("You have been kicked from the game");
+        navigate("/battleship");
+      } else {
+        getGame.refetch();
+      }
+    });
+
+    return () => {
+      socket.off("playerJoined");
+      socket.off("playerLeft");
+      socket.off("playerKicked");
+    };
+    //eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [socket, gameRoomDetails?.player1.id]);
 
   const isHost = user?.id === gameRoomDetails?.player1.id;
   const canStartGame = gameRoomDetails?.player2 && isHost;
